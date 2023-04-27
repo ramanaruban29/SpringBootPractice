6f2ab96589dc8aff017c583f14ed3d971e669494
Ramanaruban <ramanaruban29@gmail.com>
2023-04-27 12:15:35 +0530

    Update Employee.java
    Error in Employee model bugged

--- SpringBootApp2/src/main/java/com/net/javaguides/spring/model/Employee.java
+++ SpringBootApp2/src/main/java/com/net/javaguides/spring/model/Employee.java
@@ -25,4 +25,38 @@ public class Employee {
 	
 	@Column(name = "email")
 	private String email;
+public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = emailId;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String emailId) {
+		this.email = emailId;
+	}
 }
